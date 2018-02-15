8470395f108baa019077cb8e72313a7df094cc58
Carlos Yunda <cyunda@uniandes.edu.co>
2018-02-14 20:21:01 -0500

    Program Assignment 2
    Vector Example adapted to Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Example procedure adapted to solve function and matrix object. Only works on 3x3 matrices.
 
-## Write a short comment describing this function
+## same structure as vector but adapted for matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) i <<- solve
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Again, the code is adapted to the solve function and matrix object.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  n = nrow(data)
+  b<-diag(,n)
+  i <- solve(data, b, ...)
+  x$setinv(i)
+  i
+  
 }
